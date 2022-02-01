8ee78482bb3b9e0a9982f00f968577afb493418b
Kelvin Henrique Ferreira <94548044+kelvinhenri99@users.noreply.github.com>
2022-02-01 07:33:26 -0300

    Importação do código

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function Mycolor() {
+    var element = document.getElementById("myID");
+    element.style.backgroundColor='rgb(34, 34, 34)';
+    element.style.color='white';
+    element.style.borderColor="white";
+}
